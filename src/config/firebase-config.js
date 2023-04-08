32366e4e2e4770fcf2471d4da172ac6bd233e9db
Kamil Słomiński <kam_slomka@wp.pl>
2023-04-08 13:34:28 +0200

    Setup project

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDDJFq662oX2O7l2Y_p36FcSU96R1nxHUY',
+  authDomain: 'fir-v9-guide.firebaseapp.com',
+  projectId: 'fir-v9-guide',
+  storageBucket: 'fir-v9-guide.appspot.com',
+  messagingSenderId: '1057838534170',
+  appId: '1:1057838534170:web:e2871d7fcdd2c5da9ccba3',
+  measurementId: 'G-XMLPHTH4LY',
+};
+
+const app = initializeApp(firebaseConfig);
