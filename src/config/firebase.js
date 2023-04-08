2359cdaf5d6bf7041fb053d4ee29fb3cea109a21
Kamil Słomiński <kam_slomka@wp.pl>
2023-04-08 15:03:55 +0200

    Show movie list

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDDJFq662oX2O7l2Y_p36FcSU96R1nxHUY',
@@ -14,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
+
+export const db = getFirestore(app);
